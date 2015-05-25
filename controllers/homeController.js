13010b17cffeae9aab9de2bbbc4884ff49d3d63a
Phil Hack <phil@philhack.com>
2015-05-25 20:58:43 +0100

    added a controller and route mapping to the project in a modular way

---
+++ controllers/homeController.js
@@ -0,0 +1,9 @@
+(function(homeController){
+
+    homeController.init = function(app){
+        app.get('/', function (req, res) {
+            res.render('index', {title: 'Express + Vash'});
+        });
+    }
+
+})(module.exports);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+var controllers = require('./controllers');
+
 //app.set('view engine', 'jade');    // for using the Jade View Engine
 //var ejsEngine = require('ejs-locals');
 //app.engine('ejs', ejsEngine);   // support master pages
@@ -9,9 +11,9 @@ var app = express();
 
 app.set('view engine', 'vash');
 
-app.get('/', function (req, res) {
-    res.render('index', {title: 'Express + Vash'});
-});
+
+// map the routes
+controllers.init(app);
 
 app.get('/api/users', function (req, res) {
     res.set('Content-Type', 'application/json');     // not nessary, but an example of setting headers
