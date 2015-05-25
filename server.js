8f8a38f46bf4c03eb0a5ed9445625434e4528297
Phil Hack <phil@philhack.com>
2015-05-25 15:24:50 +0100

    setup express and configured it to send back json for specific uris

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    console.log(req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<html><body><h1>' + req.url + '</h1></body></html>')
-    res.end();
-}).listen(1337, '127.0.0.1');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send('<html><body><h1>Express</h1></body></html>');
+});
+
+app.get('/api/users', function(req, res){
+    res.set('Content-Type','application/json');     // not nessary, but an example of setting headers
+   res.send({ name: 'Phil', isValid: true, group: 'Admin'});    // express knows we really want this as json and sends us json back
+});
+
+var server = http.createServer(app);
+server.listen(1337, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:1337/');
