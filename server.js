1392b24628445099c30b3701e110afcf2debea19
Phil Hack <phil@philhack.com>
2015-05-25 11:48:04 +0100

    switched web server to write the url to the browser in html

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body><h1>' + req.url + '</h1></body></html>')
+    res.end();
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
