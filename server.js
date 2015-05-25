7a2115f55c9ea22553d6d15968e48ab4eb0c4b56
Phil Hack <phil@philhack.com>
2015-05-25 19:23:17 +0100

    added ejs view engine

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'jade');
-app.get('/', function(req, res){
-   //res.send();
-    res.render('jade/index', { title: 'Express + Jade'});
+//app.set('view engine', 'jade');    // for using the Jade View Engine
+var ejsEngine = require('ejs-locals');
+app.engine('ejs', ejsEngine);   // support master pages
+app.set('view engine', 'ejs');  // ejs view engine
+
+
+app.get('/', function (req, res) {
+    res.render('ejs/index', {title: 'Express + EJS'});
 });
 
-app.get('/api/users', function(req, res){
-    res.set('Content-Type','application/json');     // not nessary, but an example of setting headers
-   res.send({ name: 'Phil', isValid: true, group: 'Admin'});    // express knows we really want this as json and sends us json back
+app.get('/api/users', function (req, res) {
+    res.set('Content-Type', 'application/json');     // not nessary, but an example of setting headers
+    res.send({name: 'Phil', isValid: true, group: 'Admin'});    // express knows we really want this as json and sends us json back
 });
 
 var server = http.createServer(app);
