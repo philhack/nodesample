55b18491b69a92e4986b84f43fcb951ebcad4851
Phil Hack <phil@philhack.com>
2015-05-25 11:01:13 +0100

    sample requiring directories

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ console.log(x * y);
 var msgs = require('./msgs.js');    // this is only loaded once. so any subsequent request return a cached version
 msg = new msgs();
 console.log(msg.first);
+
+
+var logger = require('./logger');
+logger.log('this is from the logger');
