44bd4c5d6bb836133f9d6c47c64956516d5e97ff
Phil Hack <phil@philhack.com>
2015-05-25 15:31:14 +0100

    added jade view engine and sample jade template for the index

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
 app.get('/', function(req, res){
-   res.send('<html><body><h1>Express</h1></body></html>');
+   //res.send();
+    res.render('jade/index', { title: 'Express + Jade'});
 });
 
 app.get('/api/users', function(req, res){
